66baba45ea2e30c9a368a8d5bb486a641b3e3300
Thomas <taulo@live.dk>
2022-02-28 20:14:59 +0100

    Lektion 06: Opgave 5.1 - 5.6

---
+++ Lektion06/Lektion06/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lektion06
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void ClickEvent(Object sender, RoutedEventArgs e)
+        {
+            // opgave 6.4
+            if (checkBox.IsChecked == true)
+            {
+                topBtn.IsEnabled = false;
+            } else
+            {
+                topBtn.IsEnabled = true;
+            }
+
+            // opgave 6.5
+            var tempBatman = batman.Content;
+            var tempRobin = robin.Content;
+
+            batman.Content = hulk.Content;
+            robin.Content = superman.Content;
+            hulk.Content = tempBatman;
+            superman.Content = tempRobin;
+        }
+
+        private void ClickEvent2(Object sender, RoutedEventArgs e)
+        {
+            // opgave 6.5
+            var tempHulk = hulk.Content;
+            var tempSuperman = superman.Content;
+
+            hulk.Content = batman.Content;
+            superman.Content = robin.Content;
+            batman.Content = tempHulk;
+            robin.Content = tempSuperman;
+        }
+
+    }
+}
---
+++ Lektion06_forsat/Lektion06_forsat/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lektion06_forsat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickEvent(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string content = (sender as Button).Content.ToString();
+            switch (content)
+            {
+                case "Vest":
+                    textBox.AppendText("vest");
+                    textBox.AppendText(Environment.NewLine);
+                    break;
+                case "Nord":
+                    textBox.AppendText("Nord");
+                    textBox.AppendText(Environment.NewLine);
+                    break;
+                case "Øst":
+                    textBox.AppendText("Øst");
+                    textBox.AppendText(Environment.NewLine);
+                    break;
+                case "Syd":
+                    textBox.AppendText("Syd");
+                    textBox.AppendText(Environment.NewLine);
+                    break;
+            }
+        }
+    } 
+}
