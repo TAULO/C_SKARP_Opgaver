070a0c1af6b8ac30a89c1595ca029c792d9ac2fb
Thomas <taulo@live.dk>
2022-03-02 19:53:12 +0100

    Førdiggjort opgave 6.8

--- Lektion06_forsat/Lektion06_6_8/MainWindow.xaml.cs
+++ Lektion06_forsat/Lektion06_6_8/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,84 @@ namespace Lektion06_6_8
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        Label labelLemon;
+        Label labelOrange;
+        Label labelBanana;
+        Label labelApple;
+        private void CheckBox_Checked_Lemon(object sender, RoutedEventArgs e)
+        {
+            var currCheckbox = e.Source as CheckBox;
+            labelLemon = new Label();
+            if (currCheckbox != null)
+                labelLemon.Content = currCheckbox.Content;
+                stackPanelCheckboxes.Children.Add(labelLemon);
+        }
+
+        private void CheckBox_Checked_Orange(object sender, RoutedEventArgs e)
+        {
+            var currCheckbox = e.Source as CheckBox;
+            labelOrange = new Label();
+            if (currCheckbox != null)
+                labelOrange.Content = currCheckbox.Content;
+                stackPanelCheckboxes.Children.Add(labelOrange);
+        }
+
+        private void CheckBox_Checked_Banana(object sender, RoutedEventArgs e)
+        {
+            var currCheckbox = e.Source as CheckBox;
+            labelBanana = new Label();
+            if (currCheckbox != null)
+                labelBanana.Content = currCheckbox.Content;
+                stackPanelCheckboxes.Children.Add(labelBanana);
+        }
+
+        private void CheckBox_Checked_Apple(object sender, RoutedEventArgs e)
+        {
+            var currCheckbox = e.Source as CheckBox;
+            labelApple = new Label();
+            if (currCheckbox != null)
+                labelApple.Content = currCheckbox.Content;
+                stackPanelCheckboxes.Children.Add(labelApple);
+        }
+
+        private void CheckBox_Unchecked_Lemon(object sender, RoutedEventArgs e)
+        {
+            stackPanelCheckboxes.Children.Remove(labelLemon);
+        }
+
+        private void CheckBox_Unchecked_Orange(object sender, RoutedEventArgs e)
+        {
+            stackPanelCheckboxes.Children.Remove(labelOrange);
+        }
+
+        private void CheckBox_Unchecked_Banana(object sender, RoutedEventArgs e)
+        {
+            stackPanelCheckboxes.Children.Remove(labelBanana);
+        }
+
+        private void CheckBox_Unchecked_Apple(object sender, RoutedEventArgs e)
+        {
+            stackPanelCheckboxes.Children.Remove(labelApple);
+        }
+
+        Label radioLabel;
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var currRadioBtn = e.Source as RadioButton; 
+            if (currRadioBtn != null)
+            {
+                radioLabel = new Label();
+                radioLabel.Content = currRadioBtn.Content;
+                stackPanelRadio.Children.Add(radioLabel);
+            }
+        }
+
+        private void RadioButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            stackPanelRadio.Children.Remove(radioLabel);
         }
     }
 }
