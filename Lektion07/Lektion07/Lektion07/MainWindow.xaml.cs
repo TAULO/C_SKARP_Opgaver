94df62f4c3fb2f289a65239c941815803bfce4e5
Thomas Taulo <taulo@live.dk>
2022-03-03 11:53:34 +0100

    Startede på opgave 7.1 lektion 7

---
+++ Lektion07/Lektion07/Lektion07/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Lektion07_1;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lektion07
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person p;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            p = new Person("", 0.0, 0, 0, false);
+            mainGrid.DataContext = p;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var name = textBoxName.Text;
+            var weigth = textBoxWeigth.Text;
+            var age = textBoxAge.Text;
+            var score = textBoxScore.Text;
+            bool accepted = false;
+            p = new Person(name, double.Parse(weigth), int.Parse(age), int.Parse(score), accepted);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Trace.WriteLine(p.ToString());
+        }
+    }
+}
---
+++ Lektion07/Lektion07/Lektion07/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lektion07_1
+{
+    internal class Person
+    {
+        private string name; 
+        private double weight;
+        private int age;
+        private int score;
+        private bool accepted;
+
+        public Person(string name, double weight, int age, int score, bool accepted)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.age = age;
+            this.score = score;
+            this.accepted = accepted;
+        }
+
+        public string Name { set; get; }
+        public double Weight { get; set; }
+        public int Age { get; set; }    
+        public bool Score { get; set; } 
+        public bool Accept { get; set; }
+
+        public override string ToString()
+        {
+            return this.name + " " +
+                   this.weight + " " +
+                   this.age + " " +
+                   this.score + " " +
+                   this.accepted;
+        }
+
+    }
+}
